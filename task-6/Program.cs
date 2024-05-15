3f340d444a21b65b779fea03bd65c41efb96088f
Husniddin Tugizov <husniddindev@gmail.com>
2024-05-16 00:12:29 +0500

    Mohirdev task

---
+++ task-6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class ParityCheck
+{
+    static void Main()
+    {
+        Console.WriteLine("Raqamni kiriting:");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if (num % 2 == 0)
+        {
+            Console.WriteLine("Juft");
+        }
+        else
+        {
+            Console.WriteLine("Toq");
+        }
+    }
+}
